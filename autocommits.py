c0135d2d880cbb3801d050d2cf92f32e29644736
Israel Glixinski <israel_glixinski@hotmail.com>
2024-12-31 15:16:26 -0300

    .

--- autocommits.py
+++ autocommits.py
@@ -35,7 +35,7 @@ def git_commit(repo_path, commit_message="Commit automático"):
         subprocess.run(["git", "merge", "main", ], check=True)
         subprocess.run(["git", "add", "."], check=True)
         subprocess.run(["git", "commit", ], check=True)
-        subprocess.run(["git", "push", "origin", "main2", ], check=True)
+        subprocess.run(["git", "push", "github", "main2", ], check=True)
         subprocess.run(["git", "checkout", "main", ], check=True)
         
         

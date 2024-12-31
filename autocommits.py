47b8dedc10a2efafa3684a8b36731fa773c85848
Israel Glixinski <israel_glixinski@hotmail.com>
2024-12-31 16:28:13 -0300

    Atualização do código - 2024-12-31 16:28:13

--- autocommits.py
+++ autocommits.py
@@ -16,16 +16,16 @@ log_register('...')
 def git_commit(repo_path, commit_message="Commit automático"):
     try:
         os.chdir(repo_path)
-        subprocess.run(["git", "add", "."], check=True)
-        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        complete_message = f"{commit_message} - {timestamp}"
-        subprocess.run(["git", "commit", "-m", complete_message], check=True)
-        subprocess.run(["git", "push", ], check=True)
-        subprocess.run(["git", "checkout", "main2", ], check=True)
-        subprocess.run(["git", "merge", "main", "-m", complete_message], check=True)
-        subprocess.run(["git", "add", "."], check=True)
-        subprocess.run(["git", "push", "github", "main2", ], check=True)
-        subprocess.run(["git", "checkout", "main", ], check=True)
+        timestamp           = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        complete_message    = f"{commit_message} - {timestamp}"
+        subprocess.run(["git", "add"        , "."                                           ], check=True)
+        subprocess.run(["git", "commit"     , "-m"      ,               complete_message    ], check=True)
+        subprocess.run(["git", "push"       ,                                               ], check=True)
+        subprocess.run(["git", "checkout"   , "main2"   ,                                   ], check=True)
+        subprocess.run(["git", "merge"      , "main"    , "-m"      ,   complete_message    ], check=True)
+        subprocess.run(["git", "add"        , "."                                           ], check=True)
+        subprocess.run(["git", "push"       , "github"  , "main2"   ,                       ], check=True)
+        subprocess.run(["git", "checkout"   , "main"    ,                                   ], check=True)
     except Exception as e:
         pass
 

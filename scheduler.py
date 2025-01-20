4cbce42af30a4910fb5379ac1f3bca1a8441fff4
Israel Glixinski <israel_glixinski@hotmail.com>
2025-01-20 14:16:59 -0300

    logic adjust

--- scheduler.py
+++ scheduler.py
@@ -1,12 +1,25 @@
 from datetime import datetime
 from time import sleep
-import orchestrator
+import common
+import obtaining_data
+import autocommits
 
 last_execute = None
+last_quote = None
+
+def execute_routine():
+    global last_quote
+    current_quote = obtaining_data.get_dollar_quote()
+    common.log_register(f'The current dollar exchange rate: R$ {current_quote:.2f}')
+    if last_quote is None or current_quote != last_quote:
+        last_quote = current_quote
+        repository_path = "C:\\Projetos\\git_duplo\\anabele"
+        commit_message = "Log update"
+        autocommits.git_commit(repository_path, commit_message)
 
 while True:
     if last_execute is None or (datetime.now() - last_execute).seconds >= 3600:
         last_execute = datetime.now()
-        orchestrator.execute_routine()
+        execute_routine()
     sleep(60)
     pass

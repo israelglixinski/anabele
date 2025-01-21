d264454820de361239235224d32840edfb3dbe4c
Israel Glixinski <israel_glixinski@hotmail.com>
2025-01-21 17:19:23 -0300

    Log update - 2025-01-21 17:19:23

--- scheduler.py
+++ scheduler.py
@@ -1,14 +1,15 @@
 from datetime import datetime
+from threading import Thread
 from time import sleep
-import common
 import obtaining_data
 import autocommits
+import common
 
 last_execute = None
 last_quote = None
 
 def execute_routine():
-    global last_quote
+    global last_quote, last_execute
     current_quote = obtaining_data.get_dollar_quote()
     common.log_register(f'The current dollar exchange rate: R$ {current_quote:.2f}')
     if last_quote is None or current_quote != last_quote:
@@ -17,9 +18,12 @@ def execute_routine():
         commit_message = "Log update"
         autocommits.git_commit(repository_path, commit_message)
 
-while True:
-    if last_execute is None or (datetime.now() - last_execute).seconds >= 3600:
-        last_execute = datetime.now()
-        execute_routine()
-    sleep(60)
-    pass
+def execute_routine_thread():
+    global last_quote, last_execute
+    while True:
+        if last_execute is None or (datetime.now() - last_execute).seconds >= 3600:
+            last_execute = datetime.now()
+            execute_routine()
+        sleep(60)
+
+Thread(target=execute_routine_thread).start()

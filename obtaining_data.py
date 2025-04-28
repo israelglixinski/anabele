cc7cc3581dc77b28e1cb2f7785d23033a9d9bdf9
Israel Glixinski <israel_glixinski@hotmail.com>
2025-04-28 13:30:46 -0300

    Log update - 2025-04-28 13:30:46

--- obtaining_data.py
+++ obtaining_data.py
@@ -1,11 +1,23 @@
 import requests
 import json
 
+# def get_dollar_quote():
+#     url = 'https://economia.awesomeapi.com.br/json/last/USD-BRL'
+#     response = requests.get(url)
+#     datar = response.json()
+#     print(datar)
+#     dollar_exchange = datar['USDBRL']['bid']
+#     return float(dollar_exchange)
+
 def get_dollar_quote():
-    url = 'https://economia.awesomeapi.com.br/json/last/USD-BRL'
+    url = 'https://api.exchangerate.fun/latest?base=USD'
     response = requests.get(url)
-    datar = response.json()
-    dollar_exchange = datar['USDBRL']['bid']
-    return float(dollar_exchange)
+    data = response.json()
+    return float(data['rates']['BRL'])
 
 
+if __name__ == '__main__':
+    get_dollar_quote()
+
+
+    pass
